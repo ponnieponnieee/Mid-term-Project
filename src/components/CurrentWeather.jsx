8a570d3d9a6ab48871bc42ce3e857cb02dc4c4ca
Bui Huu Phuoc <phuocbui3102@gmail.com>
2025-03-16 02:58:21 +0700

    Complete for tablet, not complete for iphone 14 pro max because I can't change on web

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './CurrentWeather.css';
 
 const CurrentWeather = ({ city, temp, condition, high, low, unit, convertTemperature }) => {
-    // Chuyển đổi các giá trị nhiệt độ theo đơn vị
-    const convertedTemp = convertTemperature(temp, unit);
-    const convertedHigh = convertTemperature(high, unit);
-    const convertedLow = convertTemperature(low, unit);
+  // Chuyển đổi các giá trị nhiệt độ theo đơn vị
+  const convertedTemp = convertTemperature(temp, unit);
+  const convertedHigh = convertTemperature(high, unit);
+  const convertedLow = convertTemperature(low, unit);
   
-    return (
-      <div className="current-temp-container">
-        <h1>{city}</h1>
-        <h2>{convertedTemp}°{unit === "metric" ? "C" : "F"}</h2> {/* Hiển thị nhiệt độ theo đơn vị */}
-        <p>{condition}</p>
-        <p>H: {convertedHigh}°{unit === "metric" ? "C" : "F"} - L: {convertedLow}°{unit === "metric" ? "C" : "F"}</p>
-      </div>
-    );
+  return (
+    <div className={`current-temp-container`}>
+      <h1>{city}</h1>
+      <h2>{convertedTemp}°{unit === "metric" ? "C" : "F"}</h2>
+      <p>{condition}</p>
+      <p>H: {convertedHigh}°{unit === "metric" ? "C" : "F"} - L: {convertedLow}°{unit === "metric" ? "C" : "F"}</p>
+    </div>
+  );
 };
 
 export default CurrentWeather;
-

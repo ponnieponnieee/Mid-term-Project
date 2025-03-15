74137ebfcd6beb81339360d56c718393a4a0e87d
ponnieponnieee <phuongthanh7893@gmail.com>
2025-03-15 12:20:15 +0700

    cập nhật icon trong additionalinfo

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -1,42 +1,49 @@
 import React from "react";
 import "./AdditionalInfo.css";
+import { FaWind } from "react-icons/fa";
+import { MdVisibility } from "react-icons/md";
+import { FaCloud } from "react-icons/fa";
+import { RiWaterPercentFill } from "react-icons/ri";
+import { FaTemperatureThreeQuarters } from "react-icons/fa6";
+import { FaGauge } from "react-icons/fa6";
+
+
 
 const AdditionalInfo = ({ humidity, pressure, windSpeed, cloudiness, visibility, feelsLike, convertTemperature, unit}) => {
     return (
         <div className="additional-info">
             <div className="info-box">
-                <h3>💨 <br/>
-                    WIND</h3>
+                <h3><FaWind /><br/> WIND</h3>
                 <p>{windSpeed ? `${windSpeed} m/s` : "Loading..."}</p>
             </div>
 
             <div className="info-box">
-                <h3> 👁️ VISIBILITY</h3>
+                <h3><MdVisibility /><br/> VISIBILITY</h3>
                 <p>{visibility ? `${visibility / 1000} km` : "Loading..."}</p>
             </div>
 
             <div className="info-box cloudiness">
-                <h3> ☁️ CLOUDINESS</h3>
+                <h3><FaCloud /><br/> CLOUDINESS</h3>
                 <p>{cloudiness !== null ? `${cloudiness}%` : "Loading..."}</p>
             </div>
 
             <div className="info-box">
-                <h3> 💧 HUMIDITY</h3>
+                <h3><RiWaterPercentFill /> <br/>HUMIDITY</h3>
                 <p>{humidity ? `${humidity}%` : "Loading..."}</p>
             </div>
 
             <div className="info-box">
-    <h3>🌡️ <br/> FEELS LIKE</h3>
-    <p>
-        {feelsLike !== null 
-            ? `${convertTemperature(feelsLike, unit)}°${unit === "metric" ? "C" : "F"}`
-            : "Loading..."}
+                <h3><FaTemperatureThreeQuarters /><br/>FEELS LIKE</h3>
+                <p>
+                    {feelsLike !== null 
+                    ? `${convertTemperature(feelsLike, unit)}°${unit === "metric" ? "C" : "F"}`
+                    : "Loading..."}
     </p>
 </div>
 
 
             <div className="info-box">
-                <h3>⚖ PRESSURE</h3>
+                <h3><FaGauge /> PRESSURE</h3>
                 <p>{pressure ? `${pressure} hPa` : "Loading..."}</p>
             </div>
         </div>
